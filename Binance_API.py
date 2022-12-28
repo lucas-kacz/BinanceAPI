a593e48ee3dd80ed92ec801006b7385ce760c7c1
Lucas Kaczmarski <lucaskacz1@gmail.com>
2022-12-28 18:49:22 +0100

    Update Python

--- Binance_API.py
+++ Binance_API.py
@@ -3,15 +3,41 @@ import json
 import hmac
 import hashlib
 import requests
+import os
+import sqlite3
+from sqlite3 import Error
 from urllib.parse import urljoin, urlencode
 
-API_KEY = "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A"
-SECRET_KEY = "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
+API_KEY = ""
+SECRET_KEY = ""
 BASE_URL = "https://api.binance.com"
+
+database_path = "C:/Users/Lucas Kaczmarski/OneDrive - De Vinci/Documents/Esilv/A4/SqLite Databases/td9.db"
 
 headers = {
     'X-MBX-APIKEY': API_KEY
 }
+
+con = sqlite3.connect(os.path.abspath(database_path))
+cur = con.cursor()
+
+for row in cur.execute('''SELECT * FROM my_table'''):
+    print(row)
+
+
+# def create_connection(database_path):
+#     """ create a database connection to the SQLite database
+#         specified by db_file
+#     :param db_file: database file
+#     :return: Connection object or None
+#     """
+#     conn = None
+#     try:
+#         conn = sqlite3.connect(database_path) 
+#     except Error as e:
+#         print(e)
+
+#     return conn
 
 
 PATH='/api/v1/time'
@@ -84,7 +110,7 @@ def getDepth(direction, pair):
     else:
         raise BinanceException(status_code=r.status_code, data=r.json())
 
-#getDepth('asks', 'BTCUSDT')
+getDepth('asks', 'BTCUSDT')
 
 def orderBook(pair):
     PATH = '/api/v3/depth'
@@ -127,12 +153,38 @@ def getCandle(pair, interval):
         #     print(type(t[direction]))
         #     # print(d[0])
         #     # print(type(d[0]))
+        date = t[0][0]
+        high = t[0][2]
+        low = t[0][3]
+        open = t[0][1]
+        close = t[0][4]
+        volume = t[0][5]
 
+        print(f'''{date}''')
+
+        sql = f'''INSERT INTO my_table(
+            {date}, 
+            {high}, 
+            {low}, 
+            {open}, 
+            {close}, 
+            {volume}
+            );'''
+        cur.execute(f"""INSERT INTO my_table(date,high,low,open,close,volume)
+            VALUES(
+            {date}, 
+            {high}, 
+            {low}, 
+            {open}, 
+            {close}, 
+            {volume}
+            );""")
+        con.commit()
 
     else:
         raise BinanceException(status_code=r.status_code, data=r.json())
 
-#getCandle('BTCUSDT', '5m')
+getCandle('BTCUSDT', '5m')
 
 # PATH = '/api/v3/depth'
 
